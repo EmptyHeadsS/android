c4554e981cec8ce6abc6b6cc4aa11cfba8143ad5
Esther Guerrier <estherguerrier1@gmail.com>
2020-01-25 21:01:06 -0500

    techniquement le login marche

---
+++ app/src/main/java/com/example/myapplication/ui/main/ComunicationInterface.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.ui.main
+
+import retrofit2.http.GET
+import retrofit2.Call
+import retrofit2.http.Query
+
+interface CommunicationService {
+    @GET("/login")
+    fun getUserConfirmation(@Query("username") username: String,
+                            @Query("password") password: String): Call<String>
+}
--- app/src/main/java/com/example/myapplication/ui/main/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/LoginFragment.kt
@@ -9,10 +9,19 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.myapplication.BuildConfig
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.fragment_main.view.*
 import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result;
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.logging.Level
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A placeholder fragment containing a simple view.
@@ -37,26 +46,31 @@ class LoginFragment : Fragment() {
 
             val username: String = frag.username.text.toString()
             val password: String = frag.password.text.toString()
+            //retr
+            // o fit
 
-//            val (request, response, result) = "localhost:3000/"
-//                .httpGet()
-//                .responseString()
-//
-//            when (result) {
-//                is Result.Failure -> {
-//                    val ex = result.getException()
-//                    println(ex)
-//                }
-//                is Result.Success -> {
-//                    val data = result.get()
-//                    println(data)
-//                }
-//            }
+            // TODO: a changer avec nv url serveur cloud
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://localhost:3000")
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(CommunicationService::class.java)
+            val request = service.getUserConfirmation(username, password)
+
+            request.enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val data = response.body()
+                    if (data != null) {
+                        Toast.makeText(activity, "a " + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Toast.makeText(activity, "KO" + t.message, Toast.LENGTH_SHORT).show()
+                }
+            })
         }
-        /*val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })*/
+
         return frag
     }
 
