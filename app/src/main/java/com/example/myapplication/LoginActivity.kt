6ecf412cc565f95bca379dd622c68834fb078382
Asfal <alexandre.h.falardeau@gmail.com>
2020-01-26 01:20:27 -0500

    feat: spotify connection is functional

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,16 +2,19 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.ui.main.CommunicationService
+import kotlinx.android.synthetic.main.activity_main2.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -42,6 +45,14 @@ class LoginActivity : AppCompatActivity() {
                     Toast.makeText(this@LoginActivity, "KO" + t.message, Toast.LENGTH_SHORT).show()
                 }
             })
+        }
+
+        spotify_connect.setOnClickListener {
+            SpotifyService.connect(this) {
+                // val intent = Intent(this, PlayerActivity::class.java)
+                // startActivity(intent)
+                Log.println(Log.INFO, "importantYo", "Worked as planned")
+            }
         }
 
     }
